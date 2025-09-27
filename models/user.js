4ac3f17784d01338254ebe38cd0bd392bb5dbb3b
Huzaefa Ahmed <huzaefaahmed17.ju@gmail.com>
2025-09-28 01:00:18 +0530

    added passport for user authentication, created user.js for User model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+    email: { type: String, required: true, unique: true }
+});
+// passportLocalMongoose will add username, hash and salt fields to store the username, the hashed password and the salt value.
+userSchema.plugin(passportLocalMongoose); // adds username and password fields
+
+module.exports = mongoose.model('User', userSchema); 
